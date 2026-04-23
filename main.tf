f366a5c802f6b53647e5fb4e5b4efed49f9f3ec5
gaurav7879 <gmali7879@gmail.com>
2026-04-23 13:13:33 +0530

    add subnet

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "my-server" {
   ami = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
-  
+  subnet_id = "subnet-04d41fbb695e68819"
   tags = {
     Name = "Terraform server"
   }
