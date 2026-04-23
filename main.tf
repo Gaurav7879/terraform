10e3f5ef46acb26cd84806fa50833e39ee156870
gaurav7879 <gmali7879@gmail.com>
2026-04-23 12:52:45 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "my-server" {
+  ami = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "Terraform server"
+  }
+}
